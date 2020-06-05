c2f6eaefc19bc0275d184396144526f7b9db8d21
faeron <github@good-times.eu>
2020-06-05 22:53:06 +0200

    remove resolver in wrong file

--- src/modules/common/node.resolvers.ts
+++ src/modules/common/node.resolvers.ts
@@ -21,9 +21,4 @@ export const resolvers = {
   Account: {
     id: globalIdField("Account"),
   },
-  Mutation: {
-    signup: async (parent, args, context) => {
-      return context.account.signup(args.account);
-    },
-  },
 };
